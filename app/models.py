d3b3bdd6a3846b4c0ee59463cbeae271822502da
chucklapress <charleslapress@gmail.com>
2016-09-07 19:00:14 -0400

    changed form to regular int field

--- app/models.py
+++ app/models.py
@@ -13,7 +13,7 @@ from geoposition.fields import GeopositionField
 class EmployeeListing(models.Model):
     applicant_name = models.CharField(max_length=50)
     applicant_email = models.EmailField(max_length=30)
-    applicant_phone = models.IntegerField(unique=True, validators=[RegexValidator(regex='^\d{10}$', message='Length has to be 10', code='Invalid number')])
+    applicant_phone = models.IntegerField()
     position_applying_for = models.CharField(max_length=80)
     date_applying = models.DateTimeField(auto_now_add=True)
     post_resume_or_cover = models.TextField(max_length=None)
