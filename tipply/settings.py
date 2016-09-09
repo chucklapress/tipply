bb336c78577e9a42e9de2ca273dff730da61e486
chucklapress <charleslapress@gmail.com>
2016-09-09 10:06:49 -0400

    still trying to collect statis

--- tipply/settings.py
+++ tipply/settings.py
@@ -118,15 +118,20 @@ AWS_HEADERS = {  # see http://developer.yahoo.com/performance/rules.html#expires
         'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
         'Cache-Control': 'max-age=94608000',
     }
-AWS_STORAGE_BUCKET_NAME = 'BUCKET_NAME'
-AWS_ACCESS_KEY_ID = 'xxxxxxxxxxxxxxxxxxxx'
-AWS_SECRET_ACCESS_KEY = 'yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy'
+aws_access_key = os.environ["aws_access_key"]
+aws_secret_key = os.environ["aws_secret_key"]
+aws_bucket = os.environ["aws_bucket"]
+
+
+S3_BUCKET_NAME = aws_bucket
+AWS_ACCESS_KEY_ID = aws_access_key
+AWS_SECRET_ACCESS_KEY = aws_secret_key
 
     # Tell django-storages that when coming up with the URL for an item in S3 storage, keep
     # it simple - just use this domain plus the path. (If this isn't set, things get complicated).
     # This controls how the `static` template tag from `staticfiles` gets expanded, if you're using it.
     # We also use it in the next setting.
-AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % S3_BUCKET_NAME
 
     # This is used by the `static` template tag from `static`, if you're using that. Or if anything else
     # refers directly to STATIC_URL. So it's safest to always set it.
