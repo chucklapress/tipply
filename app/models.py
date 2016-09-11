fd210c1dbbfd3df4dadb7b2624ea4f4ac77c3a4a
chucklapress <charleslapress@gmail.com>
2016-09-11 18:38:11 -0400

    changing settings

--- app/models.py
+++ app/models.py
@@ -12,7 +12,7 @@ from geoposition.fields import GeopositionField
 
 class EmployeeListing(models.Model):
     applicant_name = models.CharField(max_length=50)
-    applicant_image = models.FileField(upload_to='/static/images', null=True, blank=True)
+    applicant_image = models.FileField(upload_to='static/images', null=True, blank=True)
     applicant_email = models.EmailField(max_length=30)
     applicant_phone = models.IntegerField()
     position_applying_for = models.CharField(max_length=80)
